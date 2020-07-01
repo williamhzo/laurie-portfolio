2f4fb3c7fa419bde38b7d9e634c895ac40f70e83
William Hermozo <william.hermozo@protonmail.com>
2020-07-02 01:48:02 +0200

    import styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from 'react'
+
+import './styles/_global.scss'
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App"></div>
 }
 
-export default App;
+export default App
