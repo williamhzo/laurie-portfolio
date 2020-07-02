d19722788708095c2ffb4b12d5ffe9e99c5ed1aa
William Hermozo <william.hermozo@protonmail.com>
2020-07-02 19:23:34 +0200

    remove conflicting rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'jsx-a11y/accessible-emoji': 0,
     'jsx-a11y/iframe-has-title': 0,
     'no-unused-vars': 1,
-    'import/no-unresolved': 0,
+    // 'import/no-unresolved': 0,
     'no-const-assign': 1,
   },
-};
+}
