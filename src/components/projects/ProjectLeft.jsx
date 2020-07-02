95c679650b4839e49d5b4f99bac526456ee013be
William Hermozo <william.hermozo@protonmail.com>
2020-07-02 22:51:54 +0200

    add component

---
+++ src/components/projects/ProjectLeft.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Project = () => {
+  return (
+    <div className="project-container flex">
+      <div className="project-box flex centered hoverable"></div>
+      <div className="project-box flex centered"></div>
+    </div>
+  )
+}
+
+export default Project
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProjectLeft from '../components/projects/ProjectLeft'
+import ProjectRight from '../components/projects/ProjectRight'
+
+const Home = () => {
+  return (
+    <div className="main">
+      <ProjectLeft />
+      <ProjectRight />
+    </div>
+  )
+}
+
+export default Home
