b55028250fc82fa2f5da98acb5f96156692d402b
William Hermozo <william.hermozo@protonmail.com>
2020-07-02 22:51:43 +0200

    add home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import './styles/_global.scss'
-import NavigationBar from './components/NavigationBar'
+import NavigationBar from './components/navigation/NavigationBar'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <Home />
     </div>
   )
 }
