020e686545610480ce3419b351b63547c1d2d4ef
William Hermozo <william.hermozo@protonmail.com>
2020-08-01 17:42:44 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import { Switch, Route } from "react-router-dom";
 
 import './styles/_global.scss'
 import NavigationBar from './components/navigation/NavigationBar'
 import Home from './pages/Home'
 
+import StudioPranaDetail from './pages/StudioPranaDetail.jsx'
+
+
 function App() {
   return (
     <div className="App">
       <NavigationBar />
-      <Home />
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/studio-prana" component={StudioPranaDetail} />
+        </Switch>
+      </main>
     </div>
   )
 }
