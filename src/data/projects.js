9ebec7e42565fa2b59ea8bbf943e8559e63bdd94
William Hermozo <william.hermozo@protonmail.com>
2020-08-01 19:32:58 +0200

    add missing projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -35,6 +35,9 @@ const projects = {
       'set design',
       'post-production',
     ],
+    lightDescription:"À l’occasion de Rolland Garros, Lacoste organise un évènement dans son showroom parisien avec de nombreux influenceurs internationaux. La marque souhaite présenter sa dernière collection (Édition RG) au travers d’un photocall. Une collection qui se porte au sport comme à la ville.\nPour mixer ces deux univers, les répliques miniatures de célèbres monuments parisiens ont été produites à l’aide d’équipements de tennis : L’Arc de Triomphe sur les Champs-Élysées et la Pyramide du Louvre.",
+    regularDescription: "Direction Artistique, conception de l’idée créative. Réalisation d’une série d’illustrations, production des décors, Set Design et Post-production.",
+    stripColor: 'rgb(42, 51, 91)',
     backgroundColor: 'rgb(42, 51, 91)',
     largeMedia:
       'https://res.cloudinary.com/flextance/image/upload/v1594111627/laurie-bonneau-folio/HOME_LACOSTE_1_lrrsjt.png',
@@ -48,12 +51,15 @@ const projects = {
     description: '',
     emojiTags: ['💡', '🖍'],
     tags: ['direction artistique', 'illustration'],
+    lightDescription:"Série réalisée autour des personnages iconiques de l’astrologie chinoise. 10 hybrides mêlant leurs animaux totems aux 5 éléments. Dans l’ordre, les voici :\n Le Chien de Métal, la Chèvre d’Eau, le Lapin de Terre, le Dragon d’Eau, le Coq de Feu, le Buffle de Bois, le Cochon de Terre, le Rat de Métal, le Cheval d’Eau et le Serpent de Bois.",
+    regularDescription: "Conception, réalisation d’une série de visuels illustratifs.",
+    stripColor: 'rgb(228, 63, 21)',
+    backgroundColor:
+      'linear-gradient(75deg, rgb(88, 88, 86) 0%,rgb(156, 154, 154) 25%, rgb(217, 216, 216) 50%, rgb(156, 154, 154) 75%, rgb(88, 88, 86) 100%)',
     largeMedia:
       'https://res.cloudinary.com/flextance/image/upload/v1594111623/laurie-bonneau-folio/HOME_CHINESE_1_c2wt1h.png',
     smallMedia:
-      'https://res.cloudinary.com/flextance/image/upload/v1594111655/laurie-bonneau-folio/HOME_CHINESE_2_npxg1p.png',
-    backgroundColor:
-      'linear-gradient(75deg, rgb(88, 88, 86) 0%,rgb(156, 154, 154) 25%, rgb(217, 216, 216) 50%, rgb(156, 154, 154) 75%, rgb(88, 88, 86) 100%)',
+    'https://res.cloudinary.com/flextance/image/upload/v1594111655/laurie-bonneau-folio/HOME_CHINESE_2_npxg1p.png',
   },
   elsachatTv: {
     title: 'elsachat',
@@ -62,6 +68,9 @@ const projects = {
     description: '',
     emojiTags: ['🔨', '👶🏻', '💬'],
     tags: ['set design', 'identité', 'graphisme'],
+    lightDescription:"Elsa Muse et Elsa Wolinski présentent leur mini-série de Noël : ELSACHAT TV ! « Tous les jours un produit, une démo, un cadeau ! » \nCroisement d’un Télé-achat aux allures eighties avec un calendrier de l’avent aux multiples surprises, cette série diffusée sur les réseaux sociaux se décline en 8 épisodes, chacun dédié à une belle marque mode ou beauté.",
+    regularDescription: "Conception, création de l’identité visuelle et production, réalisation du Set Design.",
+    stripColor: 'rgb(106, 166, 89)',
     backgroundColor: 'rgb(106, 166, 89)',
     largeMedia:
       'https://res.cloudinary.com/flextance/video/upload/v1594111625/laurie-bonneau-folio/HOME_ELSACHAT_1_thxopd.mp4',
@@ -75,6 +84,9 @@ const projects = {
     description: '',
     emojiTags: ['💡', '👶🏻', '💬', '🖋'],
     tags: ['direction artistique', 'graphisme', 'typographie'],
+    lightDescription:"L’accompagnement bien-être ! \nPour ce tout nouvel institut, une image douce, construite autour de la main. \nAPANA, en 5 lettres comme les doigts de la main, réalise principalement des soins, manucures et massages.",
+    regularDescription: "Conception, réalisation de l’identité visuelle et des supports print. \nCréation d’une police personnalisée.",
+    stripColor: 'rgb(172, 130, 119)',
     backgroundColor: 'rgb(227, 191, 184)',
     largeMedia:
       'https://res.cloudinary.com/flextance/video/upload/v1594111618/laurie-bonneau-folio/HOME_APANA_1_uuyyzi.mp4',
@@ -88,6 +100,9 @@ const projects = {
     description: '',
     emojiTags: ['💡', '👶🏻', '💬', '🖋'],
     tags: ['direction artistique', 'graphisme', 'typographie'],
+    lightDescription:"Alain Thiénot fonde sa Maison de Champagne éponyme, en 1985, à Reims. Indépendante et familiale, la plus jeune des grandes Maisons de Champagne poursuit son développement dans un esprit résolument moderne. Pour ses réseaux, elle souhaite refléter une image symbole de qualité, de modernité et de créativité, emblèmes de ses passion et vision.",
+    regularDescription: "Direction Artistique du réseau social Instagram de la Maison Thiénot pour l’année 2018. Conception, création d’une série de visuels. Confection des décors et réalisation photographique.",
+    stripColor: 'rgb(255, 255, 255)',
     backgroundColor: 'rgb(141, 111, 135)',
     largeMedia:
       'https://res.cloudinary.com/flextance/image/upload/v1594111617/laurie-bonneau-folio/galette_home_mln7ii.png',
@@ -101,6 +116,9 @@ const projects = {
     description: '',
     emojiTags: ['🖥'],
     tags: ['post-production'],
+    lightDescription:"Pour ses nouveaux sacs, Christian Louboutin désire mettre à l’honneur 6 artisans portugais et leur savoir-faire. \nTout d’abord, en créant deux précieux sacs cabas en édition limitée avec eux. Puis en réalisant une vidéo sur son odyssée au Portugal, leur rencontre, leur collaboration et leur talent à tous.",
+    regularDescription: "Réalisation de la Post-production.",
+    stripColor: 'rgb(103, 141, 243)',
     backgroundColor: 'rgb(255, 209, 25)',
     largeMedia:
       'https://res.cloudinary.com/flextance/image/upload/v1594112226/laurie-bonneau-folio/HOME_LOUBOUTIN_1-min_iqzx45.png',
@@ -114,6 +132,9 @@ const projects = {
     description: '',
     emojiTags: ['🧃', '💡', '🖍'],
     tags: ['packaging', 'direction artistique', 'illustration'],
+    lightDescription:"La Scandinavie est l’un des meilleurs marchés des cosmétiques Biotherm. La marque désire lui déclarer son amour ! À cette fin, elle lui dédie une édition limitée de sa collection phare de laits corporels : Biotherm loves Scandinavia.",
+    regularDescription: "Réalisation d’une série d’illustrations et conception des packagings.",
+    stripColor: 'rgb(17, 85, 156)',
     backgroundColor: 'rgb(232, 188, 182)',
     largeMedia:
       'https://res.cloudinary.com/flextance/image/upload/v1594111619/laurie-bonneau-folio/HOME_BIO_1_wuqupp.png',
@@ -127,6 +148,9 @@ const projects = {
     description: '',
     emojiTags: ['🔨', '🖥'],
     tags: ['set design', 'post-production'],
+    lightDescription:"Personnage de fiction haut en couleur, Jean-Louis, c’est le nouvel ambassadeur des salons Who’s Next et Première Classe. Il y présente une quarantaine de ses créateurs chouchous. Et pour l’occasion, afin de distraire toute sa bande, il désire un photocall rock et cosmique à son image.",
+    regularDescription: "Conception, réalisation d’une série de visuels en Stop Motion, élaboration du Set Design et Post-production.",
+    stripColor: 'rgb(102, 90, 162)',
     backgroundColor: 'rgb(93, 93, 95)',
     largeMedia:
       'https://res.cloudinary.com/flextance/video/upload/v1594111614/laurie-bonneau-folio/AURIELLE_JEANLOUIS_huxlyw.mp4',
@@ -140,12 +164,15 @@ const projects = {
     description: '',
     emojiTags: ['💡', '🔨', '💬', '🖍'],
     tags: ['direction artistique', 'set design', 'graphisme', 'illustration'],
+    lightDescription:"À l’occasion de la Résidence d’été MADE.COM à l’hôtel Molitor, un appartement pop-up de 150m2 fut confié à plusieurs créatifs. Chacun s’est vu attribuer une pièce du logement éphémère, avec l’objectif d’en faire un showroom créatif et immersif. \nLe placard d’Elsa, de 4m2, fut transformé puis gradé au rang de capsule spatiale pour faire voguer le navire Molitor jusqu’au soleil : LA COCKPIT ROOM !",
+    regularDescription: "Conception d’un univers, production de visuels de communication et réalisation du Set Design de la Room.",
+    stripColor: 'rgb(219, 236, 221)',
+    backgroundColor:
+      'linear-gradient(90deg, rgba(252,192,13,1) 25%, rgba(227,6,19,1) 100%)',
     largeMedia:
       'https://res.cloudinary.com/flextance/image/upload/v1594111630/laurie-bonneau-folio/HOME_MADE_1_oipxek.jpg',
     smallMedia:
       'https://res.cloudinary.com/flextance/image/upload/v1594111669/laurie-bonneau-folio/HOME_MADE_2_wbyfpj.png',
-    backgroundColor:
-      'linear-gradient(90deg, rgba(252,192,13,1) 25%, rgba(227,6,19,1) 100%)',
   },
   canalPlus: {
     title: 'canal +',
@@ -154,6 +181,9 @@ const projects = {
     description: '',
     emojiTags: ['🖥'],
     tags: ['post-production'],
+    lightDescription:"1M10, le tour du monde à hauteur d’yeux d’enfants ! \n1M10 est une série animée en stop motion créée par le Studio Mumuse, pour Canal+ Family. Durant 40 épisodes, on suit les aventures de Romy et Alessio, deux enfants qui voyagent depuis leur chambre avec leur imagination.",
+    regularDescription: "Réalisation de la Post-production.",
+    stripColor: '34, 107, 90)',
     backgroundColor: 'rgb(34, 107, 90)',
     largeMedia:
       'https://res.cloudinary.com/flextance/image/upload/v1594111621/laurie-bonneau-folio/HOME_CANAL_1_lbcbfn.png',
